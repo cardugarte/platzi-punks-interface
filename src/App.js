26474b7f8fc59c947c8bf471f9929ec70fd2e1f2
Carlos Dugarte <soycarlosdugarte@gmail.com>
2022-12-17 11:47:31 -0300

    Se agrega la conexion con Metamask

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+// import { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './views/home'
+import Web3 from "web3/dist/web3.min";
 
 function App() {
+  // // Request account access if needed with useEffect
+  // useEffect(() => {
+  //   if(window.ethereum) {
+  //     window.ethereum.request({ method: 'eth_requestAccounts' })
+  //       .then((account) =>console.log(account))
+  //   }
+  // }, [])
+
+  // Request account access if needed
+  const web3 = new Web3(window.ethereum)
+  web3.eth.requestAccounts()
+    .then(console.log)
+
   return (
     <Routes>
       <Route
